dc700926732c713ada1ac7f3d910591ff8b8df47
zsdlove <2096350083@qq.com>
2018-01-24 14:08:17 +0800

    seconde time

--- beautypiccrawler.py
+++ beautypiccrawler.py
@@ -6,31 +6,17 @@ import hashlib
 import sys
 import os
 import time
+import threading
+from multiprocessing import Pool, cpu_count
 header2={
     'Host': 'www.mmjpg.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
     'Accept-Encoding': 'gzip, deflate',
-    'Referer': 'http://www.mmjpg.com/mm/1193/5',
+    'Referer': 'http://www.mmjpg.com',
     'Connection': 'keep-alive',
     'Upgrade-Insecure-Requests': '1'
-}
-headers = {
-    'Host': 'www.mmjpg.com',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-    'Accept-Encoding': 'gzip, deflate',
-    'Connection': 'keep-alive'}
-header={
-     'Host': 'img.mmjpg.com',
-     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
-     'Accept': '*/*',
-     'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-     'Accept-Encoding': 'gzip, deflate',
-     'Referer': 'http://www.mmjpg.com/',
-     'Connection': 'keep-alive'
 }
 HEADERS = {
     'X-Requested-With': 'XMLHttpRequest',
@@ -38,13 +24,14 @@ HEADERS = {
                   '(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
     'Referer': "http://www.mmjpg.com"
 }
+lock=threading.Lock()
 def downloadimg(src,dirname):
     img = requests.get(src, headers=HEADERS, timeout=10)
     mm = hashlib.md5()
     filename = mm.update(src)
     filename=mm.hexdigest()
     filename=filename+".jpg"
-    basename="G://img3//"
+    basename="G://img4//"
     filedir=basename+dirname+"//"
     if os.path.exists(filedir)==False:
        os.mkdir(filedir)
@@ -65,7 +52,8 @@ def getonepageimg(url):
     response=requests.get(url)
     html2=response.content
     selector=etree.HTML(html2)
-    for num in range(1, 15):
+    with lock:
+      for num in range(1, 15):
         content=selector.xpath('/html/body/div[2]/div[1]/ul/li[' + str(num) + ']/a/img/@src')#jpg
         content2=selector.xpath('/html/body/div[2]/div[1]/ul/li['+str(num)+']/a/@href')#dir
         content4=selector.xpath("/html/body/div[2]/div[1]/ul/li["+str(num)+"]/span[1]/a/text()")#dirname
@@ -89,15 +77,19 @@ def getonepageimg(url):
               downloadimg(mmurl,content4[0])
             except:
               continue
-def main():
+if __name__ == "__main__":
     print "sssss"
-    #response=requests.get("http://www.mmjpg.com")
-    #html=response.content
-    for num in range(2, 83):
-        url=getnexturl(num)
-        print url
-        getonepageimg(url)
-timestart=time.time()
-main()
-timedf=time.time()-timestart
-print "共花费了：".decode('utf-8')+timedf+'s'
+    urls=[getnexturl(num)
+        for num in range(2,83)]
+    print urls
+    pool = Pool(processes=cpu_count())
+    try:
+       pool.map(getonepageimg,urls)
+    except Exception as e:
+      print e
+      pool.map(getonepageimg,urls)
+      getonepageimg(url)
+#timestart=time.time()
+#main()
+#timedf=time.time()-timestart
+#print "共花费了:".decode('utf-8')+str(timedf)+'s'
